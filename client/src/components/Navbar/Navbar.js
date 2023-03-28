83ca64b0db5c127dc62e411b90438821d1ea58ae
Scarlat Bogdana <bogdana.scarlat@gmail.com>
2023-03-28 12:54:57 +0300

    Refactored the code

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,15 +1,10 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { useQuery } from "@apollo/client";
-import {
-  CATEGORIES_QUERY,
-  ALL_BRANDS_QUERY,
-  BRANDS_QUERY,
-} from "../../graphql/query";
+import { CATEGORIES_QUERY } from "../../graphql/query";
 import { Link } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { applyFilters } from "../../features/products/productSlice";
-import Brands from "../Brands/Brands";
-import { clearFilters } from "../../features/products/productSlice";
+import FilterDropdown from "../FilterDropdown/FilterDropdown";
 
 //Create debounce helper function
 let clock;
@@ -110,157 +105,6 @@ const Categories = ({ classes }) => {
       </button>
     </li>
   ));
-};
-
-const FilterDropdown = () => {
-  const [isOpen, setIsOpen] = useState(false);
-  const [selectedCategory, setSelectedCategory] = useState(null);
-  const [selectedBrand, setSelectedBrand] = useState(null);
-  const [showCategories, setShowCategories] = useState(false);
-  const [showBrands, setShowBrands] = useState(false);
-
-  const dispatch = useDispatch();
-
-  const toggleDropdown = () => {
-    setIsOpen(!isOpen);
-    setShowCategories(false);
-    setShowBrands(false);
-  };
-
-  const toggleCategories = () => {
-    setShowCategories(!showCategories);
-    setShowBrands(false);
-  };
-
-  const toggleBrands = () => {
-    setShowBrands(!showBrands);
-    setShowCategories(false);
-  };
-
-  useEffect(() => {
-    return () => {
-      dispatch(clearFilters());
-    };
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
-  const handleCategoryClick = (category) => {
-    if (selectedCategory && selectedCategory.category === category) {
-      setSelectedCategory(null);
-      setSelectedBrand(null);
-      dispatch(clearFilters());
-    } else {
-      setSelectedCategory({ category, brandsClicked: false });
-      setSelectedBrand(null);
-      dispatch(applyFilters({ byCategory: category, byBrand: null }));
-    }
-  };
-
-  const handleBrandClick = (brand) => {
-    if (selectedBrand === brand) {
-      setSelectedBrand(null);
-      dispatch(
-        applyFilters({
-          byCategory: selectedCategory?.category || null,
-          byBrand: null,
-        })
-      );
-    } else {
-      setSelectedBrand(brand);
-      dispatch(
-        applyFilters({
-          byCategory: selectedCategory?.category || null,
-          byBrand: brand,
-        })
-      );
-    }
-  };
-
-  const { data, loading, error } = useQuery(CATEGORIES_QUERY, {
-    fetchPolicy: "no-cache",
-  });
-
-  const {
-    data: data2,
-    loading: loading2,
-    error: error2,
-  } = useQuery(ALL_BRANDS_QUERY, {
-    fetchPolicy: "no-cache",
-  });
-  console.log("#!#!#!@#@!");
-  console.log(data2);
-
-  if (loading || loading2) {
-    return <p>Loading...</p>;
-  }
-
-  if (error || error2) {
-    return (
-      <p>Error: {(error && error.message) || (error2 && error2.message)}</p>
-    );
-  }
-
-  const { getCategories } = data;
-  const getBrands = data2?.getBrands || [];
-  const filteredBrands = getBrands.filter(
-    (brand) => brand.category === selectedCategory?.category
-  );
-
-  const brandsToDisplay = selectedCategory ? filteredBrands : getBrands;
-  return (
-    <div className="dropdown">
-      <button
-        className="btn btn-secondary dropdown-toggle"
-        onClick={toggleDropdown}
-      >
-        Filter
-      </button>
-      {isOpen && (
-        <div className="dropdown-menu show">
-          <button className="dropdown-item" onClick={toggleCategories}>
-            Filter by category
-          </button>
-          {showCategories && (
-            <div className="dropdown-menu show">
-              {getCategories.map((category) => (
-                <button
-                  key={category}
-                  className={`dropdown-item ${
-                    selectedCategory === category ? "active" : ""
-                  }`}
-                  onClick={() => handleCategoryClick(category)}
-                >
-                  {category}
-                </button>
-              ))}
-            </div>
-          )}
-          <button className="dropdown-item" onClick={toggleBrands}>
-            Filter by brand
-          </button>
-          {showBrands && (
-            <div className="dropdown-menu show">
-              {brandsToDisplay.map((brand, index) => (
-                <button
-                  key={`${brand}_${index}`}
-                  className={`dropdown-item ${
-                    selectedBrand === brand ? "active" : ""
-                  }`}
-                  onClick={() => handleBrandClick(brand)}
-                >
-                  {brand}
-                </button>
-              ))}
-            </div>
-          )}
-
-          <button className="dropdown-item" onClick={toggleBrands}>
-            Filter by price
-          </button>
-        </div>
-      )}
-    </div>
-  );
 };
 
 const OffCanvas = () => {
--- client/src/components/SwitchBar/SwitchBar.js
+++ client/src/components/SwitchBar/SwitchBar.js
@@ -1,21 +1,32 @@
-import React from 'react'
+import React from "react";
 
-const SwitchBar = ({setProductsPerPage}) => {
+const SwitchBar = ({ setProductsPerPage }) => {
   return (
-    <nav className="navbar navbar-light bg-light mb-3 w-100" style={{maxHeight: "60px", borderRadius: "10px"}}>
+    <nav
+      className="navbar navbar-light bg-light mb-3 w-100"
+      style={{ maxHeight: "60px", borderRadius: "10px" }}
+    >
       <div className="d-flex column align-items-center">
-      <span className='ms-3'><label>Products per page</label></span>
-      <span className='ms-3'>
-        <select defaultValue={20} className="form-select" aria-label="Default select example" onChange={(e) => setProductsPerPage( Number.parseInt(e.target.value))}>
-          <option  value="10">10</option>
-          <option  value="20">20</option>
-          <option value="99999">All</option>
-        </select>
-      </span>
-    
-  </div>
-</nav>
-  )
-}
+        <span className="ms-3">
+          <label>Products per page</label>
+        </span>
+        <span className="ms-3">
+          <select
+            defaultValue={20}
+            className="form-select"
+            aria-label="Default select example"
+            onChange={(e) =>
+              setProductsPerPage(Number.parseInt(e.target.value))
+            }
+          >
+            <option value="10">10</option>
+            <option value="20">20</option>
+            <option value="99999">All</option>
+          </select>
+        </span>
+      </div>
+    </nav>
+  );
+};
 
-export default SwitchBar
+export default SwitchBar;
